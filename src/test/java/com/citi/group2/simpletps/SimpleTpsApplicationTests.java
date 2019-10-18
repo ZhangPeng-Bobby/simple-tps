a20e7570fb63a079d342b7cc256b61ab6d5a7a08
zhangpeng <1054329489@qq.com>
2019-10-18 11:38:30 +0800

    Initial commit

---
+++ src/test/java/com/citi/group2/simpletps/SimpleTpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.group2.simpletps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
