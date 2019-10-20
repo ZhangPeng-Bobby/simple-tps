bfe96344983065534f00f6cd8acc2df496269076
alo1719 <1335250440@qq.com>
2019-10-20 10:04:13 +0800

    create controller

--- src/main/java/com/citi/group2/simpletps/controller/tpsController.java
+++ src/main/java/com/citi/group2/simpletps/controller/tpsController.java
@@ -1,4 +1,49 @@
 package com.citi.group2.simpletps.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tps")
 public class tpsController {
+    @RequestMapping(value = "sales-leg", method = RequestMethod.POST)
+    public void insertSalesLeg() {
+
+    }
+
+    @RequestMapping(value = "trader-leg", method = RequestMethod.POST)
+    public void insertTraderLeg() {
+
+    }
+
+    @RequestMapping(value = "sales-leg", method = RequestMethod.PUT)
+    public void updateSalesLeg() {
+
+    }
+
+    @RequestMapping(value = "trader-leg", method = RequestMethod.PUT)
+    public void updateTraderLeg() {
+
+    }
+
+    @RequestMapping(value = "force-match", method = RequestMethod.POST)
+    public void forceMatch() {
+
+    }
+
+    @RequestMapping(value = "trader-leg", method = RequestMethod.GET)
+    public void getTraderLeg() {
+
+    }
+
+    @RequestMapping(value = "sales-leg", method = RequestMethod.GET)
+    public void getSalesLeg() {
+
+    }
+
+    @RequestMapping(value = "trader-login", method = RequestMethod.POST)
+    public void traderLogin() {
+
+    }
 }
