77dbecc99eabb8a32e4029e5890056f80f2c9d51
caoyu <caoyupersonal@hotmail.com>
2019-10-21 09:49:38 +0800

    recover files

---
+++ src/main/java/com/citi/group2/simpletps/SimpleTpsApplication.java
@@ -0,0 +1,13 @@
+package com.citi.group2.simpletps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTpsApplication.class, args);
+    }
+
+}
