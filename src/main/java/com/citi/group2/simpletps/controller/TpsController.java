59984a21de3b54d7a359a4f00dd64ce30edaba24
caoyu <caoyupersonal@hotmail.com>
2019-10-23 14:52:21 +0800

    Deleting ambiguous mapping and deleting legMatchTestController

--- src/main/java/com/citi/group2/simpletps/controller/TpsController.java
+++ src/main/java/com/citi/group2/simpletps/controller/TpsController.java
@@ -1,42 +1,28 @@
 package com.citi.group2.simpletps.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.citi.group2.simpletps.annotation.CurrentTrader;
 import com.citi.group2.simpletps.annotation.LoginRequired;
 import com.citi.group2.simpletps.entity.Trader;
+import com.citi.group2.simpletps.service.LegMatchService;
 import com.citi.group2.simpletps.service.TraderService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("tps")
 public class TpsController {
     private TraderService traderService;
+    private LegMatchService legMatchService;
+
     @Autowired
-    public TpsController(TraderService traderService){
+    public TpsController(TraderService traderService, LegMatchService legMatchService) {
         this.traderService = traderService;
+        this.legMatchService = legMatchService;
     }
 
-    @RequestMapping(value = "trader-leg", method = RequestMethod.POST)
-    public void insertTraderLeg() {
-
-    }
-
-    @RequestMapping(value = "trader-leg", method = RequestMethod.PUT)
-    public void updateTraderLeg() {
-
-    }
-
-    @RequestMapping(value = "force-match", method = RequestMethod.POST)
-    public void forceMatch() {
-
+    @RequestMapping(value = "force-match", method = RequestMethod.GET)
+    public Boolean forceMatch(@RequestParam Integer traderLegTxnId, @RequestParam Integer salesLegTxnId) {
+        return legMatchService.forceMatch(salesLegTxnId, traderLegTxnId);
     }
 
     @CrossOrigin
@@ -44,9 +30,9 @@ public class TpsController {
     public Object traderLogin(@RequestBody Trader trader) {
         Trader traderInDb = traderService.findById(trader.gettId());
         JSONObject json = new JSONObject();
-        if(traderInDb == null){
+        if (traderInDb == null) {
             json.put("message", "user doesn't exist");
-        }else if(!traderService.comparePassword(trader, traderInDb))
+        } else if (!traderService.comparePassword(trader, traderInDb))
             json.put("message", "wrong password");
         else {
             String token = traderService.getToken(traderInDb);
@@ -58,6 +44,6 @@ public class TpsController {
     @LoginRequired
     @GetMapping("/test")
     public Object testLog() {
-        return 9/0;
+        return 9 / 0;
     }
 }
